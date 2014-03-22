4584c1ea3b02799d0b048d91e55662b9102f4235
Katherine Zhao <mzhao12@dons.usfca.edu>
2014-03-22 01:09:21 -0700

    add R file

---
+++ homework1/homework1.R
@@ -0,0 +1,92 @@
+library(ggplot2) 
+data(movies) 
+data(EuStockMarkets)
+
+# Filter out any rows that have a budget value less than or equal to 0 in the movies dataset.
+movies <- movies[which(movies$budget > 0), ]
+
+# Add a genre column to the movies dataset as follows:
+movies$genre <- rep(NA, nrow(movies))
+count <- rowSums(movies[, 18:24])
+movies$genre[which(count > 1)] = "Mixed"
+movies$genre[which(count < 1)] = "None"
+movies$genre[which(count == 1 & movies$Action == 1)] = "Action"
+movies$genre[which(count == 1 & movies$Animation == 1)] = "Animation"
+movies$genre[which(count == 1 & movies$Comedy == 1)] = "Comedy"
+movies$genre[which(count == 1 & movies$Drama == 1)] = "Drama"
+movies$genre[which(count == 1 & movies$Documentary == 1)] = "Documentary"
+movies$genre[which(count == 1 & movies$Romance == 1)] = "Romance"
+movies$genre[which(count == 1 & movies$Short == 1)] = "Short"
+
+# Transform the EuStockMarkets dataset to a time series as follows:
+eu <- transform(data.frame(EuStockMarkets), time = time(EuStockMarkets))
+
+# Plot 1: Scatterplot. 
+movies1 <- movies
+movies1$budget_MM <- movies1$budget / 1000000
+scatterplot <- ggplot(movies1,
+                      aes(x=budget_MM, y=rating, color=genre)) + 
+                geom_point(alpha=.8) +
+#                 ggtitle("US Economic Time Series") +
+                xlab("Budget (in million)") + 
+                ylab("Rating") 
+
+print(scatterplot)
+ggsave(file = "hw1-scatter.png", plot = scatterplot, width = 8, height = 6)
+
+# Plot 2: Bar Chart.
+movies2 <- movies
+Count <- rle(sort(movies2$genre))
+movies2$Count <- Count[[1]][match(movies2$genre, Count[[2]])]
+
+barchart <- ggplot(movies,
+                   aes(x=genre, y=count)) + 
+            geom_bar(stat="identity") +
+            ggtitle("Number of Movies Per Genre") +
+            xlab("Genre") + 
+            ylab("Count")
+
+print(barchart)
+ggsave(file = "hw1-bar.png", plot = barchart, width = 8, height = 6)
+
+
+# Plot 3: Small Multiples.
+genretext <- unique(movies1$genre)
+multiples <- ggplot(movies1,
+                    aes(x=budget_MM, y=rating, color=genre))+ 
+              geom_point(alpha=.5) +
+#               ggtitle("US Economic Time Series") +
+              xlab("Budget (in million)") + 
+              ylab("Rating") +
+              scale_x_discrete(label=genretext) +
+              facet_wrap(~ genre, ncol=3)
+
+print(multiples)
+ggsave(file = "hw1-multiples.png", plot = multiples, width = 8, height = 6)
+
+
+# Plot 4: Multi-Line Chart.
+eu1 <- eu
+eu1$time <- as.numeric(time(eu1$time))
+new_eu <- data.frame(price=eu1$DAX, time=eu1$time)
+new_eu$index <- "DAX"
+new_eu <- rbind(new_eu, data.frame(price=eu1$SMI, time=eu1$time, index=rep("SMI", nrow(eu1))))
+new_eu <- rbind(new_eu, data.frame(price=eu1$CAC, time=eu1$time, index=rep("CAC", nrow(eu1))))
+new_eu <- rbind(new_eu, data.frame(price=eu1$FTSE, time=eu1$time, index=rep("FTSE", nrow(eu1))))
+
+multiline <- ggplot(new_eu,
+                    aes(x=time, y=price,
+                        group=factor(index),
+                        color=factor(index))) + 
+              geom_line() +
+              ggtitle("Time Series of Stock Market Prices for 4 indexes") +
+              xlab("Time") + 
+              ylab("Price") +
+              labs(colour="Index")
+
+print(multiline)
+ggsave(file = "hw1-multiline.png", plot = multiline, width = 8, height = 6)
+
+
+
+
