90f5e754ab243a03b768f64381804a670973b226
Katherine Zhao <mzhao12@dons.usfca.edu>
2014-04-15 16:47:07 -0700

    uploading dataset

---
+++ project-dataset/data_clean.R
@@ -0,0 +1,10 @@
+setwd("/Users/Katherine/GitHub/msan622/project-dataset/namesbystate")
+library(plyr)
+listOfFiles <- list.files(pattern= ".TXT") 
+# Read in all txt files.
+d <- do.call(rbind, lapply(listOfFiles, read.table, sep = ",", 
+                           col.names=c("State", "Sex", "Year", "Name", "Number"), 
+                           fill=FALSE, strip.white=TRUE)) 
+# Sub set the data by extracting only the data from 2002 to 2012.
+sub_d <- subset(d, Year >= 2002)
+write.csv(sub_d, file = "NamesOf50States.csv")
